72f4a5da1a73bf51fe353d55eb5855b0fe8bd86c
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-05-29 19:40:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 document.querySelector('main#main').remove();
 
 var newHeader = document.createElement('h1');
@@ -6,4 +7,9 @@ newHeader.setAttribute('id', 'victory')
 
 newHeader.innerHTML = '<h1>victory</h1> is the champion';
 
+=======
+document.getElementById('main').remove();
+const newHeader = document.createElement('H1');
+newHeader.id = "victory";
+>>>>>>> 2c05029b07be882a7286b43be0060964887f542b
 newHeader.innerText = 'YOUR-NAME is the champion';
